f5adbc0571db76f237fb9cafddc00d9adbe9b8f9
Jesse Abadilla <18732253+nothingnesses@users.noreply.github.com>
2023-07-21 19:24:14 +0100

    First version of card, without semantic information

--- HOUR-35/src/App.jsx
+++ HOUR-35/src/App.jsx
@@ -1,6 +1,116 @@
 function App() {
 	return (
 		<>
+			<div class="bg-gray-300 p-[1rem] grid grid-flow-col justify-evenly justify-items-center items-center w-full gap-[1rem]">
+				{/* Card 1 */}
+				<article class="bg-white rounded-[8px] overflow-hidden">
+					{/* White Section */}
+					<div class="ml-[30px] mr-[30px] mt-[30px] grid grid-flow-row">
+						{/* Open Quotes */}
+						<svg
+							width="35"
+							height="28"
+							viewBox="0 0 35 28"
+							fill="none"
+							class="mb-[20px]"
+						>
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M11.6396 25.9401C10.3596 27.2601 8.83961 27.9201 7.07961 27.9201C5.23961 27.9201 3.69961 27.2201 2.45961 25.8201C1.21961 24.4201 0.599609 22.6001 0.599609 20.3601C0.599609 16.5201 1.77961 12.7001 4.13961 8.90006C6.49961 5.10006 9.35961 2.32006 12.7196 0.560059L15.3596 5.12006C13.1196 6.72006 11.3996 8.24006 10.1996 9.68006C8.99961 11.1201 8.27961 12.8801 8.03961 14.9601C9.55961 15.0401 10.8596 15.7201 11.9396 17.0001C13.0196 18.2801 13.5596 19.7601 13.5596 21.4401C13.5596 23.1201 12.9196 24.6201 11.6396 25.9401ZM30.4196 25.9401C29.0996 27.2601 27.5596 27.9201 25.7996 27.9201C24.0396 27.9201 22.5396 27.2001 21.2996 25.7601C20.0596 24.3201 19.4396 22.5201 19.4396 20.3601C19.4396 16.5201 20.5996 12.7001 22.9196 8.90006C25.2396 5.10006 28.1196 2.32006 31.5596 0.560059L34.0796 5.12006C31.8396 6.64006 30.1396 8.14006 28.9796 9.62006C27.8196 11.1001 27.1596 12.8801 26.9996 14.9601C28.4396 15.1201 29.6996 15.8401 30.7796 17.1201C31.8596 18.4001 32.3996 19.8401 32.3996 21.4401C32.3996 23.1201 31.7396 24.6201 30.4196 25.9401Z"
+								fill="#1DA5AB"
+							/>
+						</svg>
+						{/* Text */}
+						<div class="mb-[max(0px,_calc(-10%_+_80px))]">
+							<p class="mb-[1rem]">
+								My mom would always say give credit where it is due. Honestly,
+								this agency deserves an award!
+							</p>
+							<p class="mb-[1rem]">
+								My consultant, Ed Hyland has been informative, friendly,
+								supportive and knowledgeable from the beginning. I am now proud
+								to say that I successfully obtained my visa and BRP, and have
+								settled in quite nicely to my new role…
+							</p>
+						</div>
+						{/* Close Quotes */}
+						<div class="grid items-end justify-items-end h-fit">
+							<svg width="35" height="28" viewBox="0 0 35 28" fill="none">
+								<path
+									fill-rule="evenodd"
+									clip-rule="evenodd"
+									d="M23.0401 2.54004C24.3201 1.22005 25.8401 0.560043 27.6001 0.560043C29.4401 0.560043 30.9801 1.26004 32.2201 2.66004C33.4601 4.06004 34.0801 5.88004 34.0801 8.12004C34.0801 11.96 32.9001 15.78 30.5401 19.58C28.1801 23.38 25.3201 26.16 21.9601 27.92L19.3201 23.36C21.5601 21.76 23.2801 20.24 24.4801 18.8C25.6801 17.36 26.4001 15.6 26.6401 13.52C25.1201 13.44 23.8201 12.76 22.7401 11.48C21.6601 10.2 21.1201 8.72005 21.1201 7.04004C21.1201 5.36004 21.7601 3.86004 23.0401 2.54004ZM4.26008 2.54004C5.58008 1.22005 7.12008 0.560043 8.88008 0.560043C10.6401 0.560043 12.1401 1.28004 13.3801 2.72004C14.6201 4.16004 15.2401 5.96004 15.2401 8.12004C15.2401 11.96 14.0801 15.78 11.7601 19.58C9.44008 23.38 6.56008 26.16 3.12008 27.92L0.600079 23.36C2.84008 21.84 4.54008 20.34 5.70008 18.86C6.86008 17.38 7.52008 15.6 7.68008 13.52C6.24008 13.36 4.98008 12.64 3.90008 11.36C2.82008 10.08 2.28008 8.64005 2.28008 7.04004C2.28008 5.36004 2.94008 3.86004 4.26008 2.54004Z"
+									fill="#1DA5AB"
+								/>
+							</svg>
+						</div>
+					</div>
+					{/* Blue Section */}
+					<div class="">
+						{/* Decoration */}
+						<svg
+							width="407.44095"
+							height="53"
+							viewBox="0 0 407.44095 53.000001"
+							preserveAspectRatio="xMinYMin meet"
+							class="w-full h-full"
+						>
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M 407.44094,53.000019 0,0 v 149.17212 c 0,4.418 3.581729,8 7.999999,8 H 399.44094 c 4.419,0 8,-3.582 8,-8 z"
+								fill="#031a89"
+							/>
+						</svg>
+						{/* Content */}
+						<div class="bg-[#031A89] relative min-h-[100px]">
+							<div class="absolute grid grid-flow-col justify-between items-end pl-[30px] pr-[30px] pb-[40px] bottom-0 w-full">
+								{/* Profile */}
+								<div class="grid grid-flow-row items-center justify-items-center w-fit">
+									{/* Image */}
+									<img
+										src="/src/assets/stock-photo-cheerful-multiracial-professional-business-people-laughing-together-standing-in-row-near-wall-1477336913.png"
+										alt="Profile image"
+										class="rounded-full h-full w-auto mb-[20px]"
+									/>
+									{/* Name */}
+									<div class="font-days-one text-[20px] font-[400] text-white">
+										Rashida
+									</div>
+								</div>
+								{/* Link */}
+								<a
+									href="https://example.org/"
+									class="grid grid-flow-col items-center justify-items-center w-fit h-fit"
+								>
+									<span
+										class="font-catamaran font-[900] text-[18px] text-white mr-[8.5px] w-fit"
+										aria-label="Read on"
+									>
+										Read on
+									</span>
+									<div class="rounded-full bg-white bg-opacity-[0.25]">
+										<svg
+											width="25"
+											height="25"
+											viewBox="0 0 25 25"
+											fill="none"
+											role="img"
+											aria-label="Arrow Icon"
+										>
+											<path
+												d="M6.25 13.6719C5.81853 13.6719 5.46875 13.3221 5.46875 12.8906C5.46875 12.4592 5.81853 12.1094 6.25 12.1094V13.6719ZM19.5312 12.1094C19.9627 12.1094 20.3125 12.4592 20.3125 12.8906C20.3125 13.3221 19.9627 13.6719 19.5312 13.6719V12.1094ZM18.9914 12.3259C19.3033 12.0277 19.7979 12.0389 20.096 12.3508C20.3941 12.6627 20.383 13.1572 20.0711 13.4554L18.9914 12.3259ZM14.7586 18.5335C14.4467 18.8316 13.9521 18.8205 13.654 18.5086C13.3559 18.1967 13.367 17.7021 13.6789 17.404L14.7586 18.5335ZM20.0711 12.3259C20.383 12.624 20.3941 13.1186 20.096 13.4305C19.7979 13.7424 19.3033 13.7535 18.9914 13.4554L20.0711 12.3259ZM13.6789 8.37724C13.367 8.0791 13.3559 7.58457 13.654 7.27267C13.9521 6.96077 14.4467 6.94962 14.7586 7.24776L13.6789 8.37724ZM6.25 12.1094H19.5312V13.6719H6.25V12.1094ZM20.0711 13.4554L14.7586 18.5335L13.6789 17.404L18.9914 12.3259L20.0711 13.4554ZM18.9914 13.4554L13.6789 8.37724L14.7586 7.24776L20.0711 12.3259L18.9914 13.4554Z"
+												fill="white"
+											/>
+										</svg>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</article>
+			</div>
 		</>
 	);
 }
